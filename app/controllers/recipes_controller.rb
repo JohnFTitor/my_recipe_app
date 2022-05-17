bcb24d3970635f47ed75cae9d39a4a3850ebbce3
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-17 16:28:09 -0500

    Add functionality to redirect when no authenticated

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,6 @@
 class RecipesController < ApplicationController
-
+  before_action :authenticate_user!
+  
+  def index
+  end
 end
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Recipes", type: :request do
+  
   describe "GET /index" do
-    
+    it 'should redirect to login page if not authenticated' do
+      get recipes_path
+
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(new_user_session_path)
+    end
   end
 end
