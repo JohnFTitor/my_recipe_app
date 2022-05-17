2d278c104f28c5144d95d290738fca3980af6dcc
Leonardo Albornoz <albornozleo95@gmail.com>
2022-05-17 13:44:52 -0400

    Create food model and define the relationships

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,4 @@
-class Recipe < ApplicationRecord
+class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_17_172638) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_17_173525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.money "price", scale: 2
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
@@ -44,5 +54,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_17_172638) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
   add_foreign_key "recipes", "users"
 end
