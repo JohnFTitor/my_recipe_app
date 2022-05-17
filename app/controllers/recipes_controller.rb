3062d0066ec9f7aee13acf16d19f60c58497bcd4
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-17 16:06:22 -0500

    Create recipes controller
    - Change data-turbo properties on devise views

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,3 @@
+class RecipesController < ApplicationController
+
+end
---
+++ spec/requests/recipes_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Recipes", type: :request do
+  describe "GET /index" do
+    
+  end
+end
