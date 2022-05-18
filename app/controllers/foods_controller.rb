abd54ca7f83bdd0393e59c778683baa72b9abe64
Leonardo Albornoz <albornozleo95@gmail.com>
2022-05-18 18:46:03 -0400

    Add create and delete methods with validation

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,12 +1,31 @@
 class FoodsController < ApplicationController
+  before_action :authenticate_user!
 
   #/foods
   def index
     @items = most_recent_added
   end
 
+  #/foods (post)
   def create
+    food = Food.new(foods_params)
+    food.user = current_user
 
+    if food.save
+      flash[:success] = 'Food has been created'
+    else
+      flash[:alert] = 'Error'
+    end
+      redirect_to foods_path
+  end
+
+  def destroy
+    if Food.find(params[:id]).destroy 
+      flash[:success] = 'Food has been deleted'
+    else
+      flash[:alert] = 'Error'
+    end
+      redirect_to foods_path
   end
 
   # public: gets the most recent posts from the database
@@ -19,6 +38,6 @@ class FoodsController < ApplicationController
   private
 
   def  foods_params
-    params[]
+    params[:foods].permit(:name, :measurement_unit, :price)
   end
 end
