1c09cef62ffec0e289bdc12a630d882bf726dec1
Leonardo Albornoz <albornozleo95@gmail.com>
2022-05-20 19:31:04 -0400

    Create styles for toggle button, recipes form, and modal

---
+++ app/controllers/recipe_foods_controller.rb
@@ -0,0 +1,33 @@
+class RecipeFoodsController < ApplicationController
+  def create
+    food = Food.find_by(name: recipe_foods_params[:ingredient])
+    recipe = Recipe.find(recipe_foods_params[:recipe])
+    ingredient = RecipeFood.new(food:, recipe:, quantity: recipe_foods_params[:quantity])
+
+    if ingredient.save
+      flash[:success] = 'Ingredient item has been created'
+    else
+      flash[:alert] = 'Ingredient item could not be saved'
+    end
+    redirect_back fallback_location: '/'
+  end
+
+  def destroy
+    respond_to do |format|
+      format.html do
+        if RecipeFood.find(params[:id]).destroy
+          flash[:success] = 'Recipe food has been deleted'
+        elsif
+          flash[:alert] = 'Recipe food could not be deleted'
+        end
+        redirect_back fallback_location: '/'
+      end
+    end
+  end
+
+  private
+
+  def recipe_foods_params
+    params.require(:ingredient).permit(:ingredient, :quantity, :recipe)
+  end
+end
